ac4c72ecea34f22ed1790ccc996731232203a412
Emily Davenport <emilydavenport410@u.boisestate.edu>
2025-09-23 09:38:36 -0600

    Adding implemented modules

--- lab-05/top.v
+++ lab-05/top.v
@@ -1,1 +1,25 @@
-// Implement top level module
+module top(
+    input [6:0]sw,
+    output [1:0]led
+);
+    wire X;
+
+    circuit_a a_inst(
+        .A(sw[0]),
+        .B(sw[1]),
+        .C(sw[2]),
+        .D(sw[3]),
+        .Y(X)
+    );
+    
+    assign led[0] = X;
+    
+    circuit_b b_inst(
+        .A(X),
+        .B(sw[4]),
+        .C(sw[5]),
+        .D(sw[6]),
+        .Y(led[1])
+    );
+    
+endmodule
