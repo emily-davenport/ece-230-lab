9959ad8e3c77ae19b1a861a02a1fa772406bf93d
Emily Davenport <emilydavenport410@u.boisestate.edu>
2025-09-30 09:49:26 -0600

    Completing lab 5

--- lab-06/adder.v
+++ lab-06/adder.v
@@ -2,8 +2,11 @@ module adder(
     // Declare your A/B inputs
     // Declare Y output
     // Declare carry output
+    input A, B,
+    output Y, Carry
 );
 
-    // Enter logic equation here
-
+    assign Y = A ^ B;
+    assign Carry = A & B;
+    
 endmodule
--- lab-06/top.v
+++ lab-06/top.v
@@ -1,1 +1,37 @@
-// Implement top level module
+module top(
+    input [0:7]sw,
+    output [0:5]led
+);
+
+    light light_inst(
+        .downstairs(sw[0]),
+        .upstairs(sw[1]),
+        .stair_light(led[0])
+    );
+    
+    adder half_adder_inst(
+        .A(sw[2]),
+        .B(sw[3]),
+        .Y(led[1]),
+        .Carry(led[2])
+     );
+     
+     wire C;
+     
+     full_adder full_adder_inst_1(
+        .A(sw[4]),
+        .B(sw[6]),
+        .Cin(0),
+        .Y(led[3]),
+        .Cout(C)
+     );
+     
+     full_adder full_adder_inst_2(
+        .A(sw[5]),
+        .B(sw[7]),
+        .Cin(C),
+        .Y(led[4]),
+        .Cout(led[5])
+     );
+
+endmodule
